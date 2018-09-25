75bf6ec9bb40457e94ac69fc94faa21eab08ebad
Matt Cadorette <mcadorette@gmail.com>
2018-09-25 12:39:43 -0400

    Initial commit from Atomist

---
+++ src/main/java/green1/Green1Application.java
@@ -0,0 +1,12 @@
+package green1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Green1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Green1Application.class, args);
+	}
+}
